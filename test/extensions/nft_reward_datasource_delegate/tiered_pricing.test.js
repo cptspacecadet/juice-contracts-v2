2bfd34296b2dd7bfd69b7528e20356a34d1db7a3
cptspacecadet <m@dao-lawfirm.xyz>
2022-06-12 01:24:26 -0400

    - more tests

--- test/extensions/nft_reward_datasource_delegate/tiered_pricing.test.js
+++ test/extensions/nft_reward_datasource_delegate/tiered_pricing.test.js
@@ -67,7 +67,7 @@ describe('NFTRewardDataSourceDelegate::didPay(...)', function () {
     };
   }
 
-  it(`Should mint token if meeting tier 1 contribution parameters`, async function () {
+  it('Should mint token if meeting tier 1 contribution parameters', async () => {
     const { jbNFTRewardDataSource, projectTerminal, beneficiary } = await setup();
 
     await expect(jbNFTRewardDataSource.connect(projectTerminal).didPay({
@@ -83,7 +83,7 @@ describe('NFTRewardDataSourceDelegate::didPay(...)', function () {
     })).to.emit(jbNFTRewardDataSource, 'Transfer').withArgs(ethers.constants.AddressZero, beneficiary.address, 1);
   });
 
-  it(`Should mint token if exceeding tier 3 contribution parameters`, async function () {
+  it('Should mint token if exceeding tier 3 contribution parameters', async () => {
     const { jbNFTRewardDataSource, projectTerminal, beneficiary } = await setup();
 
     await expect(jbNFTRewardDataSource.connect(projectTerminal).didPay({
@@ -99,7 +99,7 @@ describe('NFTRewardDataSourceDelegate::didPay(...)', function () {
     })).to.emit(jbNFTRewardDataSource, 'Transfer').withArgs(ethers.constants.AddressZero, beneficiary.address, 1501);
   });
 
-  it(`Should mint token if meeting tier 3 contribution parameters`, async function () {
+  it('Should mint token if meeting tier 3 contribution parameters', async () => {
     const { jbNFTRewardDataSource, projectTerminal, beneficiary } = await setup();
 
     await expect(jbNFTRewardDataSource.connect(projectTerminal).didPay({
@@ -115,7 +115,7 @@ describe('NFTRewardDataSourceDelegate::didPay(...)', function () {
     })).to.emit(jbNFTRewardDataSource, 'Transfer').withArgs(ethers.constants.AddressZero, beneficiary.address, 1501);
   });
 
-  it(`Should not mint token if below min contribution`, async function () {
+  it('Should not mint token if below min contribution', async () => {
     const { jbNFTRewardDataSource, projectTerminal, beneficiary } = await setup();
 
     await expect(jbNFTRewardDataSource.connect(projectTerminal).didPay({
@@ -129,5 +129,41 @@ describe('NFTRewardDataSourceDelegate::didPay(...)', function () {
       memo: '',
       metadata: '0x42'
     })).not.to.emit(jbNFTRewardDataSource, 'Transfer');
+  });
+
+  it('fail to deploy: INVALID_PRICE_SORT_ORDER', async () => {
+    const { deployer } = await setup();
+    const unsortedRewardTiers = [
+      { contributionFloor: tier1Floor, idCeiling: 1001, remainingAllowance: 1000 },
+      { contributionFloor: tier3Floor, idCeiling: 1511, remainingAllowance: 10 },
+      { contributionFloor: tier2Floor, idCeiling: 1501, remainingAllowance: 500 }
+    ];
+
+    const nftRewardTieredPriceResolverFactory = await ethers.getContractFactory('NFTRewardTieredPriceResolver', deployer);
+
+    await expect(nftRewardTieredPriceResolverFactory.deploy(ethToken, '100000000000', 2, unsortedRewardTiers))
+      .to.be.revertedWith('INVALID_PRICE_SORT_ORDER()');
+  });
+
+  it('test non-mint results', async () => {
+    const { beneficiary, deployer } = await setup();
+    const rewardTiers = [
+      { contributionFloor: tier1Floor, idCeiling: 1001, remainingAllowance: 1000 },
+      { contributionFloor: tier2Floor, idCeiling: 1501, remainingAllowance: 500 },
+      { contributionFloor: tier3Floor, idCeiling: 1511, remainingAllowance: 10 }
+    ];
+
+    const nftRewardTieredPriceResolverFactory = await ethers.getContractFactory('NFTRewardTieredPriceResolver', deployer);
+    const nftRewardTieredPriceResolver = await nftRewardTieredPriceResolverFactory.deploy(ethToken, 0, 0, rewardTiers);
+
+    const invalidTokenContribution = { token: ethers.constants.AddressZero, value: 0, decimals: 18, currency: ethers.constants.AddressZero };
+    let tokenId = await nftRewardTieredPriceResolver.callStatic.validateContribution(beneficiary.address, invalidTokenContribution, ethers.constants.AddressZero);
+
+    await expect(tokenId).to.eq(0);
+
+    const validContribution = { token: ethToken, value: 0, decimals: 18, currency: CURRENCY_ETH };
+    tokenId = await nftRewardTieredPriceResolver.callStatic.validateContribution(beneficiary.address, validContribution, ethers.constants.AddressZero);
+
+    await expect(tokenId).to.eq(0);
   });
 });
