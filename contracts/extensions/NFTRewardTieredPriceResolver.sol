bafb5e6369417252d6d51afbb0bc6321ce3f5fc6
cptspacecadet <m@dao-lawfirm.xyz>
2022-06-17 12:02:48 -0400

    - doc

--- contracts/extensions/NFTRewardTieredPriceResolver.sol
+++ contracts/extensions/NFTRewardTieredPriceResolver.sol
@@ -5,9 +5,20 @@ import '../structs/JBTokenAmount.sol';
 import '../interfaces/extensions/IPriceResolver.sol';
 import '../interfaces/extensions/ITokenSupplyDetails.sol';
 
+/**
+  @dev Token id 0 has special meaning in NFTRewardDataSourceDelegate where minting will be skipped.
+  @dev An example tier collecting might look like this:
+  [ { contributionFloor: 1 ether, idCeiling: 1001, remainingAllowance: 1000 }, { contributionFloor: 1 ether, idCeiling: 1501, remainingAllowance: 500 }, { contributionFloor: 1 ether, idCeiling: 1511, remainingAllowance: 10 }]
+ */
 struct RewardTier {
+  /** @notice Minimum contribution to qualify for this tier. */
   uint256 contributionFloor;
+  /** @notice Highest token id in this tier. */
   uint256 idCeiling;
+  /**
+    @notice Remaining number of tokens in this tier.
+    @dev Together with idCeiling this enables for consecutive, increasing token ids to be issued to contributors.
+  */
   uint256 remainingAllowance;
 }
 
@@ -24,14 +35,14 @@ contract NFTRewardTieredPriceResolver is IPriceResolver {
   error INVALID_ID_SORT_ORDER(uint256);
 
   /**
-    @notice blah
+    @notice This price resolver allows project admins to define multiple reward tiers for contributions and issue NFTs to people who contribute at those levels. It is also possible to limit total number of NFTs issues and total number of NFTs issued per account regardless of the contribution amount. Let's say the total number of NFTs defined in the tiers is 10k, the global mint cap can limit that number to 5000 across all tiers.
 
     @dev Tiers list must be sorted by floor otherwise contributors won't be rewarded properly.
 
-    @param _contributionToken blah
-    @param _mintCap blah
-    @param _userMintCap blah
-    @param _tiers blah
+    @param _contributionToken Token used for contributions, use JBTokens.ETH to specify ether.
+    @param _mintCap Global mint cap, this allows limiting total NFT supply in addition to the limits already defined in the tiers.
+    @param _userMintCap Per-account mint cap.
+    @param _tiers Sorted tier collection.
    */
   constructor(
     address _contributionToken,
@@ -60,12 +71,12 @@ contract NFTRewardTieredPriceResolver is IPriceResolver {
   }
 
   /**
-    @notice blah
+    @notice Returns the token id that should be minted for a given contribution for the contributor account.
 
-    @dev blah
+    @dev If token id 0 is returned, the mint should be skipped. This function specifically does not revert so that it doesn't interrupt the contribution flow since NFT rewards are optional and may be exhausted during project or funding cycle lifetime.
 
-    @param account blah
-    @param contribution blah
+    @param account Address sending the contribution.
+    @param contribution Contribution amount.
     @param token Reward token to be issued as a reward, used to read token data only.
    */
   function validateContribution(
